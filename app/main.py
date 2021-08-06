53f0186c96abbd32316e499b5c7271ea73ce8283
Kerri-Leigh Grady <klgrady@gmail.com>
2021-08-06 11:37:15 -0400

    Adding poets

--- app/main.py
+++ app/main.py
@@ -16,13 +16,20 @@ def get_asteroid():
     death_dict = resp.json()
     todays_death = death_dict['near_earth_objects'][this_day]
     
-    ret_string = "There are " + str(death_dict['element_count']) + " near-Earth objects right now!<p>"
+    ret_string = "There are " + str(death_dict['element_count']) + " near-Earth objects right now! Lucky us!<p>Choose your favorite object of destruction:"
 
     for item in todays_death:
-        ret_string += "<a href=\"" + item["nasa_jpl_url"] + "\">Near Earth Object " + item['name'] + "</a><BR>"
+        ret_string += "<a href=\"" + item["nasa_jpl_url"] + "\">Near Earth Object " + item['name'] + "</a>: <BR>"
 
+    return ret_string
+
+def get_poem():
+    URL= 'https://www.poemist.com/api/v1/randompoems'
+    resp = requests.get(URL)
+    poem_dict = resp.json()
+    ret_string = poem_dict[0]['title'] + " by " + poem_dict[0]['poet']['name'] + "<p>" + poem_dict[0]['content'].replace('\n', '<br>')
     return ret_string
 
 @app.route('/')
 def index():
-    return '<h1>How We\'ll Die Today</h1>' + get_asteroid()
+    return '<h1>How We\'ll Die Today</h1>' + get_asteroid() + "<p>No worries, though. Here's a poem to calm your nerves.<p>" + get_poem()
