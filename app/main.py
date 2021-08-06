e4dc8d18d19f4baa8b6e4baf6b898ef2a45418ed
Kerri-Leigh Grady <klgrady@gmail.com>
2021-08-06 14:14:53 -0400

    Added poetry and tried to format, but fuck css

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 import requests
 from datetime import datetime
 import sys
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 app.config.from_pyfile('config.py')
 KEY = app.config["NASA_API_KEY"]
 
@@ -27,9 +29,10 @@ def get_poem():
     URL= 'https://www.poemist.com/api/v1/randompoems'
     resp = requests.get(URL)
     poem_dict = resp.json()
-    ret_string = poem_dict[0]['title'] + " by " + poem_dict[0]['poet']['name'] + "<p>" + poem_dict[0]['content'].replace('\n', '<br>')
+    ret_string = "<h3>" + poem_dict[0]['title'] + " by " + poem_dict[0]['poet']['name'] + "</h3><p>" + poem_dict[0]['content'].replace('\n', '<br>')
     return ret_string
 
 @app.route('/')
 def index():
-    return '<h1>How We\'ll Die Today</h1>' + get_asteroid() + "<p>No worries, though. Here's a poem to calm your nerves.<p>" + get_poem()
+    #return '<h1>How We\'ll Die Today</h1>' + get_asteroid() + "<p>No worries, though. Here's a poem to calm your nerves.<p>" + get_poem()
+    return render_template('main.html', death = get_asteroid(), poem = get_poem())
